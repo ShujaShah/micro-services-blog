e73814ab9b45f9bbe94b8967d778a18721579dc6
ShujaShah <shuja.ashraf11@gmail.com>
2023-08-02 19:07:41 +0530

    implemented the posts and comments api

---
+++ posts/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+
+const app = express();
+app.use(bodyParser.json());
+
+const posts = {};
+
+//Route to get all the posts
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+//Route to create a post
+app.post("/posts", (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
+
+  //adding a new key
+  posts[id] = {
+    id,
+    title,
+  };
+  res.status(201).send(posts[id]);
+});
+
+app.listen(4000, () => {
+  console.log("App is listening on Port 4000 😊");
+});
