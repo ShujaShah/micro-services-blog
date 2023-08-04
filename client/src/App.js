efdde05fe058742d75cad240f4d426fd42491bc2
ShujaShah <shuja.ashraf11@gmail.com>
2023-08-04 10:11:04 +0530

    implemented post fetching through react

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
-import PostCreate from "./PostCreate";
+import PostCreate from "./components/PostCreate";
+import PostList from "./components/PostList";
 
 const App = () => {
   return (
     <div className="container">
       <h1>Create Post</h1>
       <PostCreate />
+      <hr />
+      <h2>Posts</h2>
+      <PostList />
     </div>
   );
 };
