c1b1d610504ae33e668408115aa1a9dcea4b7f32
ShujaShah <shuja.ashraf11@gmail.com>
2023-08-09 22:27:48 +0530

    refactored the code

--- query/index.js
+++ query/index.js
@@ -6,8 +6,32 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post("/posts", (req, res) => {});
-app.post("/events", (req, res) => {});
+const posts = {};
+
+//get all the posts as well as comments
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+//Receive the events from the event bus
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  //handle the event of post created
+  if (type === "PostCreated") {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+  }
+
+  //handle the event of comment created
+  if (type === "CommentCreated") {
+    const { id, content, postId } = data;
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+  console.log(posts);
+  res.send({});
+});
 
 app.listen(4002, () => {
   console.log("Listening on Port 4002 😀");
