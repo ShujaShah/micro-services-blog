9c11c0a62bb32a729cc13d5a7dd33c1a680d6b72
ShujaShah <shuja.ashraf11@gmail.com>
2023-08-21 22:29:04 +0530

    fixed the typo in posts

--- posts/index.js
+++ posts/index.js
@@ -13,12 +13,12 @@ app.use(cors());
 const posts = {};
 
 //Route to get all the posts
-app.get("/posts/create", (req, res) => {
+app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
 //Route to create a post
-app.post("/posts", async (req, res) => {
+app.post("/posts/create", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
 
